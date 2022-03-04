477001f3e5c9f7ff7a96a6822048de9a4770ad9f
Aishwarya Chikorde <41798723+aishwaryachikorde@users.noreply.github.com>
2022-03-04 13:24:33 +0530

    Update StatsUnitTest.cs

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -11,9 +11,8 @@ namespace Statistics.Test
         public void ReportsAverageMinMax()
         {
             var statsComputer = new StatsComputer();
-            var computedStats = statsComputer.CalculateStatistics(
-                new List<float>{1.5F, 8.9F, 3.2F, 4.5F});
-            float epsilon = 0.001F;
+            var computedStats = statsComputer.CalculateStatistics(new List<double>{1.5D, 8.9D, 3.2D, 4.5D});
+            double epsilon = 0.001D;
             Assert.True(Math.Abs(statsComputer.average - 4.525) <= epsilon);
             Assert.True(Math.Abs(statsComputer.max - 8.9) <= epsilon);
             Assert.True(Math.Abs(statsComputer.min - 1.5) <= epsilon);
@@ -22,8 +21,7 @@ namespace Statistics.Test
         public void ReportsNaNForEmptyInput()
         {
             var statsComputer = new StatsComputer();
-            var computedStats = statsComputer.CalculateStatistics(
-                new List<float>{0/0F,0F/0,0/0F});
+            var computedStats = statsComputer.CalculateStatistics(new List<double>{0/0D,0D/0,0/0D});
             //All fields of computedStats (average, max, min) must be
             //Double.NaN (not-a-number), as described in
             //https://docs.microsoft.com/en-us/dotnet/api/system.double.nan?view=netcore-3.1
@@ -35,9 +33,9 @@ namespace Statistics.Test
             var ledAlert = new LEDAlert();
             IAlerter[] alerters = {emailAlert, ledAlert};
 
-            const float maxThreshold = 10.2;
+            const double maxThreshold = 10.2D;
             var statsAlerter = new StatsAlerter(maxThreshold, alerters);
-            statsAlerter.checkAndAlert(new List<float>{0.2F, 11.9F, 4.3F, 8.5F});
+            statsAlerter.checkAndAlert(new List<double>{0.2D, 11.9D, 4.3D, 8.5D});
 
             Assert.True(emailAlert.emailSent);
             Assert.True(ledAlert.ledGlows);
